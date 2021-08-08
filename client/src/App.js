f1edcc92d42e1e079fb77cee60be99b86e79986c
PaulSender <paulsender99@gmail.com>
2021-08-08 11:20:53 -0400

    final code

--- client/src/App.js
+++ client/src/App.js
@@ -2,54 +2,57 @@ import React from 'react'
 import './App.css';
 import axios from 'axios'
 import { useEffect, useState } from 'react';
-import { Dropdown, Image } from 'semantic-ui-react'
+import { Button, Dropdown, Image } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 
 function App() {
-  useEffect(() => {
-    const interval = setInterval(() => {
-      axios.get('http://localhost:3001/infile').then(res => {
-        setinimage(res.data)
-      }).catch(e => {
-        console.error(e)
-      })
-      axios.get('http://localhost:3001/outfile').then(res => {
-        setoutimge(res.data)
-      }).catch(e => {
-        console.error(e)
-      })
-      axios.get('http://localhost:3001/people').then(res => {
-        setpeople(res.data)
-      }).catch(e => {
-        console.error(e)
-      })
-    }, 5000);
-    return () => clearInterval(interval)
-  }, []);
+  const [selection, setselection] = useState("live")
+  const [inimage, setinimage] = useState()
+  const [outimage, setoutimage] = useState()
+  const [status, setstatus] = useState()
+  const options = [{ key: 'live', text: 'Live', value: "live" }, { key: 'night', text: 'Night', value: "night" }, { key: 'multiple', text: 'Multiple People', value: 'multiple' }, { key: 'blury', text: 'Burry Photo', value: 'blurry' }]
+  function handleSelection(selection) {
+    if (selection === 'live') {
+      get()
+    }
+    else {
+      console.log(selection)
+      get()
+    }
+  }
+  function get() {
+    axios.get('http://localhost:3001/process', { params: { type: selection } }).then(res => {
+      const { infile, outfile, status } = res.data
+      setinimage(infile)
+      setoutimage(outfile)
+      setstatus(status)
+    }).catch(e => {
+      console.error(e)
+    })
+  }
   function handleChange(e, data) {
     setselection(data.value)
+    handleSelection(selection)
   }
-  const [inimage, setinimage] = useState()
-  const [outimage, setoutimge] = useState()
-  const [people, setpeople] = useState()
-  const [selection, setselection] = useState("live")
-  const options = [{key: 'live', text: 'Live', value: "live"}, {key: 'night', text: 'Night', value: "night"}, {key: 'multiple', text: 'Multiple People', value: 'multiple'}, {key: 'blury', text: 'Burry Photo', value: 'blurry'}]
-
+  handleSelection("live")
   return (
     <div className="App">
-      <Dropdown options={options} selection onChange={handleChange} value={selection}/>
-      {/* <div className="photoContainer">
+      <div>
+        <label>Select an example</label>
+        <br />
+        <Dropdown options={options} selection onChange={handleChange} value={selection} placeholder="Select..." />
+        {selection === 'live' && <Button basic color="grey" style={{ marginLeft: '5px' }} onClick={() => get()}>Get Latest Image</Button>}
+      </div>
+      <div className="photoContainer">
         {inimage && <Image src={`data:image/*;base64,${inimage}`} className="photo" wrapped ui={false} />}
       </div>
       <div className="photoContainer">
         {outimage && <Image src={`data:image/*;base64,${outimage}`} className="photo" wrapped ui={false} />}
       </div>
-      <div>
-        <p>People Present?</p>
-        {people && <span>{parseInt(people) > 0 ? "Yes" : "No"}</span>}
-        <p>How many?</p>
-        {people && <span>{people}</span>}
-      </div> */}
+      <div id="info">
+        <p>Fan Status</p>
+        {status}
+      </div>
     </div>
   );
 }
--- index.js
+++ index.js
@@ -6,6 +6,57 @@ const app = express()
 const port = 3001
 app.use(cors())
 
+/*
+Run the python script and collect the status output
+*/
+
+const pyProg = spawn('python', ['./capture.py']);
+var status
+pyProg.stdout.on('data', function (data) {
+  console.log(data.toString());
+  status = data.toString()
+});
+
+app.get('/process', async (req, res) => {
+  var type = req.query.type
+  var inFile
+  var outFile
+  switch (type) {
+    case 'live':
+      inFile = 'test.jpg'
+      outFile = 'testnew.jpg'
+      break
+    case 'night':
+      inFile = 'test_night.jpg'
+      outFile = 'testnew_night.jpg'
+      break
+    case 'multiple':
+      inFile = 'test_multiple.jpg'
+      outFile = 'testnew_multiple.jpg'
+      break
+    case 'blurry':
+      inFile = 'test_blurry.jpg'
+      outFile = 'testnew_blurry.jpg'
+      break
+  }
+  var processedIn = fs.readFileSync(inFile)
+  var processedOut = fs.readFileSync(outFile)
+  arrayBufferToBase64 = buffer => {
+    var binary = '';
+    var bytes = [].slice.call(new Uint8Array(buffer));
+    bytes.forEach((b) => binary += String.fromCharCode(b));
+    return window.btoa(binary);
+  };
+  var base64In = Buffer.from(processedIn).toString('base64')
+  var base64Out = Buffer.from(processedOut).toString('base64')
+  const out = {
+    infile: base64In,
+    outfile: base64Out,
+    status: status
+  }
+  res.setHeader('Content-Type', 'application/json');
+  res.send(out)
+})
 
 app.get('/infile', (req, res) => {
   fs.readFile("./test.jpg", function (err, data) {
@@ -34,12 +85,7 @@ app.get('/outfile', (req, res) => {
   })
 })
 
-const pyProg = spawn('python', ['./capture.py']);
-var people
-pyProg.stdout.on('data', function (data) {
-  console.log(data.toString());
-  people = data.toString()
-});
+
 app.get('/people', (req, res) => {
   fs.readFile("./test.jpg", function (err, data) {
     if (err) { console.error(err) }
